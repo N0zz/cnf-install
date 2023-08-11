1cbdfc1d86ad3a198b2b5905675fd37c48369021
n0zz <k.kolodziejczyk3@gmail.com>
2023-08-11 20:45:48 +0200

    move tests to docker

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 testVersion() {
-  assertEquals "Version: v0.1 (https://github.com/N0zz/cnf-install/releases/tag/v0.1)" "$(./cnf-install -v)"
+  assertEquals "Version: v0.1 (https://github.com/N0zz/cnf-install/releases/tag/v0.1)" "$(/workspace/cnf-install -v)"
 }
 
-. ./shunit2
+. /workspace//shunit2
