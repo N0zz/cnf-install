60e0f5c58c848087c244870932a65266a210f0d6
n0zz <k.kolodziejczyk3@gmail.com>
2023-08-11 22:18:05 +0200

    fix tests

--- unit_tests.sh
+++ unit_tests.sh
@@ -4,9 +4,9 @@ testVersion() {
   assertEquals "Version: v0.1 (https://github.com/N0zz/cnf-install/releases/tag/v0.1)" "$(./cnf-install -v)"
 }
 
-# testSearch() {
-#   assertContains "Installer: " "$(./cnf-install http)"
-# }
+testSearch() {
+  assertContains "$(./cnf-install http -f)" "Installer: "
+}
 
 testInstall() {
   ./cnf-install http -i
