3af9fde5ef9e87633c262f90c3d202d75e7ae41c
n0zz <k.kolodziejczyk3@gmail.com>
2023-08-11 20:38:24 +0200

    ret

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,15 +1,7 @@
 #!/bin/env bash
 
-# Source the shunit2 framework
-. shunit2
-
-# Source your script to be tested
-. cnf-install
-
-# Define your tests using functions
 testVersion() {
-  assertEquals "Version: v0.1 (https://github.com/N0zz/cnf-install/releases/tag/v0.1)" "$(cnf-install -v)"
+  assertEquals "Version: v0.1 (https://github.com/N0zz/cnf-install/releases/tag/v0.1)" "$(./cnf-install -v)"
 }
 
-# Run the tests
 . shunit2
