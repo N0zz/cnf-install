d9597e60c4a368575e659bbc3292043ba804df69
n0zz <k.kolodziejczyk3@gmail.com>
2023-08-11 21:58:38 +0200

    add help test

--- unit_tests.sh
+++ unit_tests.sh
@@ -4,4 +4,15 @@ testVersion() {
   assertEquals "Version: v0.1 (https://github.com/N0zz/cnf-install/releases/tag/v0.1)" "$(./cnf-install -v)"
 }
 
+testHelp() {
+  expected="Usage: cnf-install \${COMMAND} [-h -f -p -v -i]
+  -h / --help            - prints help/usage
+  -f / --force           - force command-not-found.com query to show installation methods even if package is already installed
+  -p / --package_only    - show package names only in output - useful for automation
+  -v / --version         - prints cnf-install version
+  -i / --install         - perform actual package installation (by default only prints installation command)"
+  
+  assertEquals "$expected" "$(./cnf-install -h)"
+}
+
 . ./shunit2
